05c12407ec29503c2e1ed3eed80434541df94b21
jigkoh3 <jigkoh3@hotmail.com>
2018-02-19 14:09:01 +0700

    2

--- modules/accounts/server/controllers/accounts.server.controller.js
+++ modules/accounts/server/controllers/accounts.server.controller.js
@@ -1261,7 +1261,7 @@ exports.returnGlreport = function (req, res) {
 
 exports.exportExcel = function (req, res) {
     var wb = new xl.Workbook();
-    var ws = wb.addWorksheet('Sheet 1');
+    var ws = wb.addWorksheet(req.daily.title);
     var numStyle = wb.createStyle({
         // font: {
         //     color: '#FF0800',
@@ -1274,17 +1274,17 @@ exports.exportExcel = function (req, res) {
 
 
 
-    ws.cell(1, 1).number(100);
-    // หมายถึงใส่ค่าตัวเลข 100 ลงไปที่ cell A1
-    ws.cell(1, 2).string('some text');
-    //หมายถึงใส่ค่าตัวอักษร some text ลงใน cell B1
-    ws.cell(1, 3).formula('A1+A2');
-    //หมายถึงใส่สูตร A1+A2 ใน cell C1
-    ws.cell(1, 4).bool(true);
-    //หมายถึงใส่ค่า boolean true ใน cell D1
-    ws.cell(1,5).number(23000).style(numStyle);
-    ws.cell(1,6).string('my big string').style({font:{ size : 25 }});
-    ws.cell(1,7).number(45900).style(numStyle).style({font:{ size : 25 }});
+    // ws.cell(1, 1).number(100);
+    // // หมายถึงใส่ค่าตัวเลข 100 ลงไปที่ cell A1
+    // ws.cell(1, 2).string('some text');
+    // //หมายถึงใส่ค่าตัวอักษร some text ลงใน cell B1
+    // ws.cell(1, 3).formula('A1+A2');
+    // //หมายถึงใส่สูตร A1+A2 ใน cell C1
+    // ws.cell(1, 4).bool(true);
+    // //หมายถึงใส่ค่า boolean true ใน cell D1
+    // ws.cell(1,5).number(23000).style(numStyle);
+    // ws.cell(1,6).string('my big string').style({font:{ size : 25 }});
+    // ws.cell(1,7).number(45900).style(numStyle).style({font:{ size : 25 }});
 
     
     wb.write('ExcelFile.xlsx', res);
