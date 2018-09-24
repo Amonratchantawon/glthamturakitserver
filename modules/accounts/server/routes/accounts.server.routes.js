a0a7a497b3cbdfd7dddb0d3afad7e944a98c4f83
jigkoh3 <jigkoh3@hotmail.com>
2018-09-24 13:36:21 +0700

    [fix] issue of perfomance in gl repory

--- modules/accounts/server/routes/accounts.server.routes.js
+++ modules/accounts/server/routes/accounts.server.routes.js
@@ -35,10 +35,10 @@ module.exports = function (app) {
     app.route('/api/glreport/:type/:date/:enddate').all(accountsPolicy.isAllowed)
         //.get(accounts.setReportCondition, accounts.getCompany, accounts.getAccounts, accounts.generateGlDaily, accounts.getAccountchart, accounts.getBringForwardForAcceach, accounts.generateAcceach, accounts.generateGain, accounts.generateBalance, accounts.balancetest, accounts.returnGlreport);
         //.get(accounts.setReportCondition, accounts.getCompany, accounts.getAccounts, accounts.generateGlDaily, accounts.getAccountchart, accounts.getBringForwardForAcceach, accounts.generateAcceach, accounts.returnGlreport);
-        .get(accounts.setReportCondition, accounts.getCompany, accounts.getAccounts, accounts.generateGlDaily, accounts.getAccountchart, accounts.getBringForwardForAcceach accounts.returnGlreport);
+        .get(accounts.setReportCondition, accounts.getCompany, accounts.getAccounts, accounts.generateGlDaily, accounts.getAccountchart, accounts.getBringForwardForAcceach, accounts.returnGlreport);
     app.route('/api/glreport/excel/:type/:date/:enddate')
         //.get(accounts.setReportCondition, accounts.getCompany, accounts.getAccounts, accounts.generateGlDaily, accounts.getAccountchart, accounts.getBringForwardForAcceach, accounts.generateAcceach, accounts.generateGain, accounts.generateBalance, accounts.balancetest, accounts.exportExcel);
-        .get(accounts.setReportCondition, accounts.getCompany, accounts.getAccounts, accounts.generateGlDaily, accounts.getAccountchart, accounts.getBringForwardForAcceach accounts.exportExcel);
+        .get(accounts.setReportCondition, accounts.getCompany, accounts.getAccounts, accounts.generateGlDaily, accounts.getAccountchart, accounts.getBringForwardForAcceach, accounts.exportExcel);
     // accounts.getGlByCondition
     // Finish by binding the Account middleware
     app.param('accountId', accounts.accountByID);
